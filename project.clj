bdca43baa615b8eb282f8143715ff5c1cb45327c
julosaure <bourdaij@iro.umontreal.ca>
2012-08-29 09:38:32 -0400

    changed dependancies

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject SduDataImporter "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"], [clojure-solr "0.4.0-SNAPSHOT"], [icarus "0.1a"]]
-  :main SduDataImporter.core)
+  :dependencies [[org.clojure/clojure "1.3.0"],
+                 [clojure-solr "0.4.0-SNAPSHOT"],
+                 ;;[icarus "0.1a"],
+                 [chisel "1.0.0-SNAPSHOT"]]
+  ;;:main SduDataImporter.core
+  )
