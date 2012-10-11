024d93a10e32624f914a5e235778eae18cbd1912
julosaure <bourdaij@iro.umontreal.ca>
2012-10-11 14:48:54 -0400

    Code cleaning.

--- src/SduDataImporter/converter.clj
+++ src/SduDataImporter/converter.clj
@@ -8,11 +8,12 @@
 
 (def COLUMN_SEPARATOR #"\|")
 
+(def NEW_LINE #"[\r\n]+")
+
 (def FIRST_COL_NAME "Activity Row Id")
 
 (def COLUMN_NAMES ["activity_row_id" "created_date" "primary_contact_id" "contact_integration_id" "case" "member" "parish_data" "start_time" "call_type" "type" "inin_id" "parish" "interaction_time" "resolution" "notes" "division" "program" "task_status" "contact_external_unique_id" "subject" "description" "contact_type" "caller_id" "crmit_inin_id" "validation_status" "worker_office_number"])
 
-(def DATE_FIELDS ["created_date" "start_time"])
 
 (def dirToProcess "/Users/julien/Documents/AfterCall/Output/")
 
@@ -21,8 +22,6 @@
 (def dirOutput  "/Users/julien/Documents/AfterCall/Output/csv/")
 
 ;; UTILS
-
- (def patDateTime #"(\d{2})/(\d{2})/(\d{4}) (\d{2}:\d{2}:\d{2})")
 
 
 ;; FUNCTIONS
@@ -37,15 +36,16 @@
     (with-open [writer (clojure.java.io/writer outFile)]
       (with-open [rdr (clojure.java.io/reader fileName)]
         (doseq [line (line-seq rdr)]
-
-          ; -1 allows to keep all trailing empty columns
-          (let [splitted (clojure.string/split line COLUMN_SEPARATOR -1)
+                
+          (let [line (clojure.string/replace line NEW_LINE "")
+                line (clojure.string/replace line #"\"" "")
+                ; -1 allows to keep all trailing empty columns
+                splitted (clojure.string/split line COLUMN_SEPARATOR -1)
                 ];_ (println (count splitted) (count COLUMN_NAMES))]
 
-            (if (= (count splitted) (count COLUMN_NAMES))
-              (.write writer (str (apply str (interpose "|" splitted)) "\n"))  
-              (println (str "Bad line" splitted "\n" line)))))))))
-
+            (if (not= (count splitted) (count COLUMN_NAMES))
+              (println (str "Bad line" splitted "\n" line))
+              (.write writer (str (apply str (interpose "|" splitted)) "\n")))))))))
 
 (defn parseDir [dirName]
   "Parse a directory"
